7375dcb111c67d28aeda95df02872cdf043aa7b7
Rishab <rishabjasrotia@github.com>
2023-09-21 20:05:49 +0530

    Add Constant Class

---
+++ src/ModuleConstants.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\drupal_timezone;
+
+class ModuleConstants {
+
+  /**
+   * Config settings.
+   *
+   * @var string
+   */
+  const SETTINGS = 'drupal_timezone.config';
+
+  /**
+   * Module Name.
+   *
+   * @var string
+   */
+  const MODULE_NAME = 'drupal_timezone';
+
+
+}
